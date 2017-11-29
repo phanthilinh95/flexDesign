646ec6b0bb4cabfff3c1027a3fea6d8c6b87ea82
unknown <tl.phan@VNNG-PC05.vnnginfotech.vn>
2017-11-29 15:50:16 +0700

    registration mobile

---
+++ js/icheck-register.js
@@ -0,0 +1,11 @@
+
+		
+		
+$(document).ready(function(){
+  $('input').iCheck({
+     checkboxClass: 'icheckbox_minimal-orange',
+    radioClass: 'iradio_minimal-orange',
+    increaseArea: '20%' // optional
+  });
+});
+	
